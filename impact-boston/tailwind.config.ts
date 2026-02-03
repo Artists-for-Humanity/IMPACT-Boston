c02e3c5a3da7183b4c25a5e0f08a3d36ada07de2
Jamie <itisjamied@gmail.com>
2026-02-03 15:12:18 -0500

    Updated with footer colors const

--- impact-boston/tailwind.config.ts
+++ impact-boston/tailwind.config.ts
@@ -10,7 +10,8 @@ const config: Config = {
           "dark-purple": "#421656", 
           "light-purple": "#6c3789",
           "orange": "#e86834",
-       
+          "black": "#0F0F0F",
+          'white': '#FFFFFF'
         },
       },
     },
