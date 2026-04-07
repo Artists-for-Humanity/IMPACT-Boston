3259cce0f0fdc8283c4135526a93432e4a439f03
Jamie <itisjamied@gmail.com>
2026-04-07 15:49:11 -0400

    Update line-heights

--- impact-boston/src/app/page.tsx
+++ impact-boston/src/app/page.tsx
@@ -1,9 +1,10 @@
-
+// all of your imports go here
 
 export default function IndexPage() {
   return (
     <main>
-  HI
+      {/* all of your main code goes in here */}
+        Start Coding in here
     </main>
   );
 }
