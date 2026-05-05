e889aa187a21bbc4e691c11187303d2b541cee16
Jamie <itisjamied@gmail.com>
2026-05-05 17:48:53 -0400

    landing blog page

---
+++ impact-boston/src/app/Blog/page.tsx
@@ -0,0 +1,112 @@
+import Hero2 from '@/components/Hero/Hero2';
+import SingleContent from '@/components/Content/Single';
+import GridContent from '@/components/Content/Grid';
+import MediaCard from '@/components/Content/MediaCard';
+
+export default function Press() {
+  return (
+    <>
+
+      <GridContent
+        title="Blogs"
+        description={[
+          "Blog publishes editorial pieces aligned with our mission, covering topics like relationship red flags, personal safety scenarios, and stories from people navigating threatening or uncomfortable situations. IMPACT Boston posts are written in a first-person or narrative style, grounded in real experiences, and tend to address the emotional and social dimensions of personal safety rather than just tactics.",
+        ]}
+      >
+        <MediaCard
+          outlet="Inside the Suit"
+          description="B Whitney, Program Coordinator, brings decades of experience teaching acting to their work at IMPACT. Their work is essential to helping our suited instructors learn their craft."
+          subtext='Jan 23, 2026  •  B Whitney'
+          href="#"
+          linkText="Read Full Article here."
+        />
+        <MediaCard
+          outlet="Making an IMPACT in Turbulent Times"
+          description="Economic stress is all around us, from the federal government shutdown to higher grocery and health care costs. We have been incredibly grateful at IMPACT for the generosity of our donors as they are helping us continue our commitment to not turning anyone away for..."
+        subtext='Oct 14, 2025  •  IMPACT Inc'
+          href="#"
+          linkText="Read Full Article here."
+        />
+        <MediaCard
+          outlet="Washington Post"
+          description="Morbid curiosity motivated me to take the National Rifle Association’s “Refuse to be a Victim” safety course last month. I arrived ready to be both appalled by LaPierre-style rant about how gun control laws have destroyed America-"
+          href="#"
+          linkText="Read Full Article here."
+        />
+        <MediaCard
+          outlet="Washington Post"
+          description="Morbid curiosity motivated me to take the National Rifle Association’s “Refuse to be a Victim” safety course last month. I arrived ready to be both appalled by LaPierre-style rant about how gun control laws have destroyed America-"
+          href="#"
+          linkText="Read Full Article here."
+        />
+         <MediaCard
+          outlet="Washington Post"
+          description="Morbid curiosity motivated me to take the National Rifle Association’s “Refuse to be a Victim” safety course last month. I arrived ready to be both appalled by LaPierre-style rant about how gun control laws have destroyed America-"
+          href="#"
+          linkText="Read Full Article here."
+        />
+           <MediaCard
+          outlet="Washington Post"
+          description="Morbid curiosity motivated me to take the National Rifle Association’s “Refuse to be a Victim” safety course last month. I arrived ready to be both appalled by LaPierre-style rant about how gun control laws have destroyed America-"
+          href="#"
+          linkText="Read Full Article here."
+        />
+           <MediaCard
+          outlet="Washington Post"
+          description="Morbid curiosity motivated me to take the National Rifle Association’s “Refuse to be a Victim” safety course last month. I arrived ready to be both appalled by LaPierre-style rant about how gun control laws have destroyed America-"
+          href="#"
+          linkText="Read Full Article here."
+        />
+           <MediaCard
+          outlet="Washington Post"
+          description="Morbid curiosity motivated me to take the National Rifle Association’s “Refuse to be a Victim” safety course last month. I arrived ready to be both appalled by LaPierre-style rant about how gun control laws have destroyed America-"
+          href="#"
+          linkText="Read Full Article here."
+        />
+           <MediaCard
+          outlet="Washington Post"
+          description="Morbid curiosity motivated me to take the National Rifle Association’s “Refuse to be a Victim” safety course last month. I arrived ready to be both appalled by LaPierre-style rant about how gun control laws have destroyed America-"
+          href="#"
+          linkText="Read Full Article here."
+        />
+           <MediaCard
+          outlet="Washington Post"
+          description="Morbid curiosity motivated me to take the National Rifle Association’s “Refuse to be a Victim” safety course last month. I arrived ready to be both appalled by LaPierre-style rant about how gun control laws have destroyed America-"
+          href="#"
+          linkText="Read Full Article here."
+        />
+           <MediaCard
+          outlet="Washington Post"
+          description="Morbid curiosity motivated me to take the National Rifle Association’s “Refuse to be a Victim” safety course last month. I arrived ready to be both appalled by LaPierre-style rant about how gun control laws have destroyed America-"
+          href="#"
+          linkText="Read Full Article here."
+        />
+           <MediaCard
+          outlet="Washington Post"
+          description="Morbid curiosity motivated me to take the National Rifle Association’s “Refuse to be a Victim” safety course last month. I arrived ready to be both appalled by LaPierre-style rant about how gun control laws have destroyed America-"
+          href="#"
+          linkText="Read Full Article here."
+        />
+           <MediaCard
+          outlet="Washington Post"
+          description="Morbid curiosity motivated me to take the National Rifle Association’s “Refuse to be a Victim” safety course last month. I arrived ready to be both appalled by LaPierre-style rant about how gun control laws have destroyed America-"
+          href="#"
+          linkText="Read Full Article here."
+        />
+           <MediaCard
+          outlet="Washington Post"
+          description="Morbid curiosity motivated me to take the National Rifle Association’s “Refuse to be a Victim” safety course last month. I arrived ready to be both appalled by LaPierre-style rant about how gun control laws have destroyed America-"
+          href="#"
+          linkText="Read Full Article here."
+        />
+           <MediaCard
+          outlet="Washington Post"
+          description="Morbid curiosity motivated me to take the National Rifle Association’s “Refuse to be a Victim” safety course last month. I arrived ready to be both appalled by LaPierre-style rant about how gun control laws have destroyed America-"
+          href="#"
+          linkText="Read Full Article here."
+        />
+      </GridContent>
+
+    </>
+  );
+}
