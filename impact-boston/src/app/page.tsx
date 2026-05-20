d27764837b1c10cdb7d3013963fe0c6a21ac65ea
Ihaverabbitstopet <wfchen30@gmail.com>
2026-05-20 17:32:17 -0400

    finished nav

--- impact-boston/src/app/page.tsx
+++ impact-boston/src/app/page.tsx
@@ -1,5 +1,6 @@
 // all of your imports go here
 import Header from "../components/Header/Header";
+import Hero1 from "../components/Hero/Hero1";
 
 export default function IndexPage() {
   return (
--- impact-boston/src/components/Header/Header.tsx
+++ impact-boston/src/components/Header/Header.tsx
@@ -3,17 +3,17 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <nav className="flex space-x-9 py-4 px-8 justify-between items-center">
+    <nav className="flex space-x-9 py-6 px-8 justify-between items-center">
       <Image
         src="/images/logos/logo-full-color.png"
         alt="logo"
         width={100}
         height={100}
       />
-      <div className="flex gap-8">
-        <div className="flex gap-4">
-          <div className="flex gap-1">
-            About
+      <div className="flex gap-8 items-center">
+        <div className="flex">
+          <div className="flex ml-[8px] mr-[8px]">
+            <div className="link text-[#000000] ml-[4px] mr-[4px]">About</div>
             <Image
               src="/icons/chevron-right.svg"
               alt="chevron-right"
@@ -21,8 +21,10 @@ export default function Header() {
               height={16}
             />
           </div>
-          <div className="flex gap-1">
-            Programs
+          <div className="flex ml-[8px] mr-[8px]">
+            <div className="link text-[#000000] ml-[4px] mr-[4px]">
+              Programs
+            </div>
             <Image
               src="/icons/chevron-right.svg"
               alt="chevron-right"
@@ -30,8 +32,10 @@ export default function Header() {
               height={16}
             />
           </div>
-          <div className="flex gap-1">
-            Learn More
+          <div className="flex ml-[8px] mr-[8px]">
+            <div className="link text-[#000000] ml-[4px] mr-[4px]">
+              Learn More
+            </div>
             <Image
               src="/icons/chevron-right.svg"
               alt="chevron-right"
@@ -44,10 +48,10 @@ export default function Header() {
           <Image src="/icons/search.svg" alt="search" width={20} height={20} />
         </div>
         <div className="flex gap-2">
-          <div className="bg-size-300 bg-purple-700 border-1 rounded-lg">
+          <div className="pb-[3px] pt-[3px] pl-[16px] pr-[16px] bg-[#000000] link text-[#ffffff] border-1 rounded-sm">
             Register
           </div>
-          <div className="bg-size-300 bg-white-500 border-1 rounded-lg">
+          <div className="pb-[3px] pt-[3px] pl-[16px] pr-[16px] bg-[#ffffff] link text-[#000000] border-1 border-[#959595] rounded-sm">
             Donate
           </div>
         </div>
