a8f526f5a004e0888bf0bdb75dfd2d6259f82f83
Ihaverabbitstopet <wfchen30@gmail.com>
2026-06-02 17:52:16 -0400

    footer half c ompleted

--- impact-boston/src/app/page.tsx
+++ impact-boston/src/app/page.tsx
@@ -2,6 +2,7 @@
 import Header from "../components/Header/Header";
 import Hero2 from "../components/Hero/Hero2";
 import Footer from "../components/Footer/Footer";
+import Image from "next/image";
 
 export default function IndexPage() {
   return (
--- impact-boston/src/components/Footer/Footer.tsx
+++ impact-boston/src/components/Footer/Footer.tsx
@@ -2,35 +2,49 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="ml-[120px] mr-[120px] mt-[88px] mb-[88px]">
-      <div className="">
-        <div className="">
-          <div className="">
-            <div className="Impact-image"></div>
-            <div className="">
-              <div className="">
-                <div className="">Address</div>
-                <div className="">89 South Street, Suite 600 Boston, MA</div>
+    <div className="pl-[120px] pr-[120px] pt-[88px] pb-[88px] flex bg-[#000000]">
+      <div className="ml-[24px] mr-[24px] w-full">
+        <div className="flex justify-between mb-[48px]">
+          <div className="w-[454px]">
+            <div className="h2 text-[#ffffff]">IMPACT</div>
+            <div className="mt-[50px]">
+              <div className="dummytest ">
+                <div className="p2 font-bold text-[#ffffff]">Address</div>
+                <div className="p2 text-[#faf6fd]">
+                  89 South Street, Suite 600 Boston, MA
+                </div>
               </div>
-              <div className="">
-                <div className="">Email</div>
-                <div className="">info@impactboston.org</div>
+              <div className="dummytest">
+                <div className="p2 font-bold text-[#ffffff]">Email</div>
+                <div className="p2 text-[#faf6fd]">info@impactboston.org</div>
               </div>
-              <div className="">
-                <div className="">Call</div>
-                <div className="">+1 617-597-4945</div>
+              <div className="dummytest">
+                <div className="p2 font-bold text-[#ffffff]">Call</div>
+                <div className="p2 text-[#faf6fd]">+1 617-597-4945</div>
               </div>
             </div>
           </div>
-          <div className="">
-            <div className="">
-              <div className="">Contact Us</div>
-              <div className="">We're here to help, reach out anytime.</div>
+          <div className="w-[465px]">
+            <div className="mb-[20px]">
+              <div className="p1 font-bold text-[#ffffff]">
+                Join our Newsletter
+              </div>
+              <div className="p1 text-[#faf6fd]">
+                We're here to help, reach out anytime.
+              </div>
             </div>
-            <div className="">Enter your email</div>
-            <div className="">
-              <div className="">Submit</div>
-              <div className="Image"></div>
+            <div className="bg-[#1F2122]">
+              <div className="p2 text-[#ffffff] pt-[21.5px] pb-[21.5px] pr-[19px] pl-[19px] opacity-40">
+                Enter your email
+              </div>
+            </div>
+            <div className="bg-[#1F2122] mt-[10px]">
+              <div className="">
+                <div className="link text-[#ffffff] pt-[19.5px] pb-[19.5px] pr-[24px] pl-[24px]">
+                  Submit
+                </div>
+                <div className=""></div>
+              </div>
             </div>
           </div>
         </div>
